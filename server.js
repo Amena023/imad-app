36380291dea7fcaf4897d5447fb30a49a6e50371
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-08-14 00:00:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send('ARTICLE ONE IS REQUESTED & WILL BE SERVED HERE')
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
     res.send('ARTICLE TWO IS REQUESTED & WILL BE SERVED HERE')
