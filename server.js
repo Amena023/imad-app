ecd9a08c0437e464ba63d66cb123e48e1ce67628
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-08-20 15:49:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/:articleName', function (req,res){
 });
 
 var names = [];
-app.get('/submit-name/:name', function() {
+app.get('/submit-name/:name', function(req,res) {
    var name = req.params.name;
    names.push(name);
    //json :js obj notation 
