8e697d366c9422eb1d088f752188d325635f593b
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-08-20 14:23:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,12 @@
-//counter code
-var button = document.getElementById('counter');
-
+var button = getElementById('counter');
 button.onclick = function() {
+    //make a req
     
-     //create request
-     var request = new XMLHttpRequest(); 
-     
-     //capture the response and store in a variable
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.Done) {
-            //Take some action
-            if (request.state === 200) {
-                     var counter = request.responseText;
-                      var span = document.getElementById('count');
-                     span.innerHTML = counter.toString();
-            }
-        }//not done
-        
-    };
-    // make a request
-    request.open('GET', 'http://amenaarif1996.imad.hasura-app.io/counter',true);
-    request.send(null);
+    //capture the res and store it in a variable;
     
+    // render the variable in the correct span
+    counter = counter + 1;
+    var span = getElementById('count');
+    span.innerHTML = counter.toString();
     
-   
-};
-//submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    // make a re to server and send the name
-    //capture a list of names and render them as alist
-    var name = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0 ; i<names.length ; i++) {
-        list = '<li>' + name[i] + '</li>';
-        
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-};
+}
