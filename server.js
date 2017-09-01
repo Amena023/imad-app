410d95439a9a07101b202bff50dda6e144d4486a
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-01 13:05:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+
 var config = {
     user :'amenaarif1996',
     database : 'amenaarif1996',
-    host : 'db.imad.hasura',
+    host : 'db.imad.hasura-app.io',
     port : '5432',
     password : process.env.DB_PASSWORD
 };
