b93db0624fc0d026790dc341dc25f011db109e34
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-01 18:20:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 var config = {
     user :'amenaarif1996',
     database : 'amenaarif1996',
@@ -12,7 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
                 
 
@@ -107,6 +109,22 @@ app.get('/hash/:input',function (req , res) {
     
 });
 
+app.post('/create-user' , function(req,res) {
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1 , $2)', [username, dbString] , function(err,result) {
+         if(err) {
+                res.status(500).send(err.toString());
+       }
+       else {
+           res.send('User Sucessfully created:' +username);
+       }
+        
+    });
+});
 
 
 
