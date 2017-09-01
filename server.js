540bfb196c0d28a49b4554dd02d7596ef70d59c2
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-01 13:19:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ res.send(JSON.stringify(comments));
 app.get('/articles/:articleName', function (req,res){
     //articleName==article-one
     //articles[articleName]=={} content object for article-one
-        pool.query("SELECT *FROM article WHERE title= $1" [req.params.articleName],function(err,result) {
+        pool.query("SELECT *FROM article WHERE title= $1", [req.params.articleName],function(err,result) {
             if(err) {
                 res.status(500).send(err.toString());
             } else { 
