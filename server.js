c39f09c7841c66ff8de412f4da6b11b80df7574d
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-01 17:37:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,14 +94,14 @@ app.get('/', function (req, res) {
 function hash (input, salt) {
     //how do we create a hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512 , 'sha512');
-    return hashed;
+    return hashed.toString('hex');
 }
 
 
 
 app.get('/hash/:input',function (req , res) {
  var  hashedString = hash(req.params.input, 'this-is-a-random-string');
- res.send(hashedString).toString('hex');
+ res.send(hashedString);
    
    
     
