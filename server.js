c88f23cc9342b429e366c360d39008cbc7f2f1a7
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-06 17:34:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,11 +94,32 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db',function(req,res) {
+ pool.query('SELECT *from test',function(err,result) {
+   if(err) {
+       res.status(500).send(err.toString());
+       }
+       else {
+           res.send(JSON.stringify(result.rows));
+       }
+ });
+});	
+
 function hash (input, salt) {
     //how do we create a hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512 , 'sha512');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
+
+app.get('/hash/:input',function (req , res) {
+ var  hashedString = hash(req.params.input, 'this-is-a-random-string');
+ res.send(hashedString);
+   
+   
+    
+});
+
 
 app.post('/create-user' , function(req,res) {
     
@@ -152,19 +173,6 @@ app.post('/login', function(req, res) {
 });
 
 
-
-
-var pool = new Pool(config);
-app.get('/test-db',function(req,res) {
- pool.query('SELECT *from test',function(err,result) {
-   if(err) {
-       res.status(500).send(err.toString());
-       }
-       else {
-           res.send(JSON.stringify(result.rows));
-       }
- });
-});	
 
  app.get('/articles/:articleName', function (req,res){
     //articleName==article-one
