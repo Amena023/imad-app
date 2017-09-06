5115de0cc258ac7e894db579e69a719c3e7ee209
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-06 16:29:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,6 +39,7 @@ submit.onclick = function() {
          if (request.readyState === XMLHttpRequest.DONE) {
     //take some action
         if(request.status === 200 ) {
+            console.log('user logged-in');
         alert('Logged in successfully');
         }
         else if (request.status === 403 ) {
@@ -53,7 +54,7 @@ submit.onclick = function() {
     console.log(username);
     console.log(password);
     // make a request
-    request.open('GET','http://amenaarif1996.imad.hasura-app.io/submit-name?name='+username,true);
+    request.open('POST','http://amenaarif1996.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
     
