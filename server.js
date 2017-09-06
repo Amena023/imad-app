311905ad3162af8070845916f1676ece41946ed2
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-06 17:47:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,18 +94,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
-app.get('/test-db',function(req,res) {
- pool.query('SELECT *from test',function(err,result) {
-   if(err) {
-       res.status(500).send(err.toString());
-       }
-       else {
-           res.send(JSON.stringify(result.rows));
-       }
- });
-});	
-
 function hash (input, salt) {
     //how do we create a hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512 , 'sha512');
@@ -119,7 +107,6 @@ app.get('/hash/:input',function (req , res) {
    
     
 });
-
 
 app.post('/create-user' , function(req,res) {
     
@@ -149,7 +136,7 @@ app.post('/login', function(req, res) {
                 res.status(500).send(err.toString());
        } else {
            if (result.rows.length === 0) {
-                res.send('Username password incorrect !!');
+                res.send(403).send('Username password incorrect !!');
            } else {
            //match the password    
            
@@ -159,18 +146,31 @@ app.post('/login', function(req, res) {
            if (hashedPassword === dbString ) {
                res.send('Credentials correct');
                //set session
-              // req.session.auth = {userid: result.rows[0].id};
+               req.session.auth = {userid: result.rows[0].id};
                //set cookie with a session id
                //inrnally on server side it maps the session id to an object
                // {auth :{userid}};
                     
            } else {
-           res.status(403).send('Username password incorrect !!');
+           res.send(403).send('Username password incorrect !!');
            }
        }
     }    
         });
 });
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res) {
+ pool.query('SELECT *from test',function(err,result) {
+   if(err) {
+       res.status(500).send(err.toString());
+       }
+       else {
+           res.send(JSON.stringify(result.rows));
+       }
+ });
+});	
+
 
 app.get('/logout' , function (req, res) {
     delete req.session.auth;
