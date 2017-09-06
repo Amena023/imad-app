9e21c7290993f0d0e60a94f39a837b3cb4b3f11b
Amena023 <30660295+Amena023@users.noreply.github.com>
2017-09-06 14:27:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,4 +123,62 @@ app.get('/', function (req, res) {
 
 });
 
+app.post('/login', function(req, res) {
+     var username = req.body.username;
+    var password = req.body.password;
+    
+    
+  pool.query('SELECT  * FROM "user" WHERE username=$1',[username], function (err,result) {
+        
+                    
+         if(err) {
+                res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+                res.send('Username password incorrect !!');
+           } else {
+           //match the password    
+           
+           var dbString = result.rows[0].password;
+           var salt = dbString.spilt('$')[2];
+           var hashPassword = hash(password, salt);
+           if (hashedPassword === dbString ) {
+               
+               //set session
+               req.session.auth = {userid: result.rows[0].id};
+               //set cookie with a session id
+               //inrnally on server side it maps the session id to an object
+               // {auth :{userid}};
+                    res.send('Credentials correct');
+           } else {
+           res.status(403).send('Username password incorrect !!');
+           }
+       }
+    }    
+        });
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
 
